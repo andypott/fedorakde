a70c4c666e59ddd3b234c033119b09a30237235c
Andy Potter <andy@andypotter.co.uk>
2023-11-08 19:24:10 +0000

    Initial commit

---
+++ home/.config/nvim/plugin/after/lsp-zero.lua
@@ -0,0 +1,43 @@
+local lsp = require("lsp-zero").preset({
+	manage_nvim_cmp = {
+		set_sources = "recommended",
+		set_extra_mappings = true,
+	},
+})
+
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({ buffer = bufnr })
+	local opts = { buffer = true }
+	vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+	vim.keymap.set("n", "<leader>=", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+end)
+
+lsp.ensure_installed({
+	"bashls",
+	"clangd",
+	"cssls",
+	"eslint",
+	"gopls",
+	"html",
+	"jsonls",
+	"lua_ls",
+	"rust_analyzer",
+	"tsserver",
+})
+
+require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+lsp.setup()
+
+local cmp = require("cmp")
+cmp.setup({
+	sources = {
+		{ name = "path" },
+		{ name = "nvim_lsp" },
+		{ name = "buffer", keyword_length = 3 },
+		{ name = "luasnip", keyword_length = 2 },
+		{ name = "neorg" },
+	},
+	mapping = {
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	},
+})
